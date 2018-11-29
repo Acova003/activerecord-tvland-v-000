798f1b6b966a7f44c75ed8fe387a05109cecd367
Amee Covarrubias <ameecova@yahoo.com>
2018-11-29 20:42:35 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
+require 'pry'
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  def full_name 
+    self.first_name + " " + self.last_name
+  end 
+  
+  def list_characters
+    self.characters.map do |characters|
+      character.name + character.show.name
+    end 
+  end 
 end
